8c4a9f8b4c9a15b95ad3e05b2f43793047dcd474
vlad-74 <kurguzov74@gmail.com>
2020-04-28 13:02:55 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -6,3 +6,16 @@ import {fromEvent} from 'rxjs';
 fromEvent(document, 'mousemove').subscribe(ev => {
   console.log('Mouse event: ', ev);
 });
+
+of('Hello').subscribe(vl => console.log(vl));
+
+const obs = new Observable(sub => {
+  sub.next(1);
+  setTimeout(() => { sub.error(3); }, 500);
+});
+
+const sub = obs.subscribe(
+  vl => console.log(vl),
+  err => console.log('Error: ', err),
+  () => console.log('Completed') // НЕ ВСЕГДА ОТРАБАТЫВАЕТ!!!!
+);
