a4aa55230152ad9a2f8deec88d687443b539b6ca
vlad-74 <kurguzov74@gmail.com>
2020-04-28 13:00:56 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import { Observable, of, from } from 'rxjs'; 
 import { map } from 'rxjs/operators';
 
- import {fromEvent} from 'rxjs';
+import {fromEvent} from 'rxjs';
 
-    fromEvent(document, 'mousemove').subscribe(ev => {
-     console.log('Mouse event: ', ev);
-    });
+fromEvent(document, 'mousemove').subscribe(ev => {
+  console.log('Mouse event: ', ev);
+});
